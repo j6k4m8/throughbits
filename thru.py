100a967f4171d54011585b430e16f4bb2291e0da
Jordan <j6k4m8@gmail.com>
2013-11-07 20:03:15 -0500

    Update thru.py

--- thru.py
+++ thru.py
@@ -5,4 +5,4 @@ os.system('git commit -am "' +
 	format(datetime.datetime.now(), "%H:%M.%S") + '"')
 os.system('git push origin master')
 os.system('git pull')
-os.system('git rebase master')
+os.system('git rebase master') # :)
