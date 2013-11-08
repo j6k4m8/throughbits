5109b910df5a5790e509b34caca002dd8c613346
= <j6k4m8@gmail.com>
2013-11-07 20:04:38 -0500

    20:04.38

--- thru.py
+++ thru.py
@@ -1,8 +1,8 @@
 import os, datetime
 
-os.system('git pull')
-os.system('git rebase master') # :)
 os.system('git add .')
 os.system('git commit -am "' +
 	format(datetime.datetime.now(), "%H:%M.%S") + '"')
+os.system('git pull')
+os.system('git rebase master') # :)
 os.system('git push origin master')
