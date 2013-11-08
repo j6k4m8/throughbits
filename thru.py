6e452fcbd1ddb2cf323285090e5e9cb56606dd5f
= <j6k4m8@gmail.com>
2013-11-07 20:26:19 -0500

    20:26.19

--- thru.py
+++ thru.py
@@ -1,8 +1,10 @@
 import os, datetime
 
+os.system('git stash')
+os.system('git pull')
+os.system('git rebase master')
+os.system('git stash apply')
 os.system('git add .')
 os.system('git commit -am "' +
 	format(datetime.datetime.now(), "%H:%M.%S") + '"')
-os.system('git pull')
-os.system('git rebase master') # :)
 os.system('git push origin master')
